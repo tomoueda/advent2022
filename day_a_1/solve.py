af7dc721bbd94954ea7a730669ae33b64b57ac7b
Tomo Ueda <tueda14@gmail.com>
2022-11-30 21:20:40 -0800

    day 1

---
+++ day_a_1/solve.py
@@ -0,0 +1,29 @@
+
+from inp import inp 
+
+# part 1
+
+m = 0
+t = 0
+for line in inp.split('\n'):
+    if line == '':
+        if t > m:
+            m = t
+        t = 0
+    else:
+        t += int(line)
+print m 
+
+# part 2
+
+
+t = 0
+cals = []
+for line in inp.split('\n'):
+    if line == '':
+        cals.append(t)
+        t = 0
+    else:
+        t += int(line)
+cals.sort(reverse=True)
+print sum(cals[:3])
